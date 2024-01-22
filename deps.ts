9abe57f5a823fa626e3e3c06bdd63528ea8d9f25
Matis <mbaguelin@expertises-galtier.fr>
2024-01-22 16:47:32 +0100

    chore: add deps.ts file

---
+++ deps.ts
@@ -0,0 +1,7 @@
+/**
+ * deps.ts
+ *
+ * This module re-exports the required methods.
+ */
+
+export { Application } from 'https://deno.land/x/oak@v12.6.2/mod.ts';
--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { Application } from 'https://deno.land/x/oak@v12.6.2/mod.ts';
+import { Application } from '@/deps.ts';
 
 const app = new Application();
 
