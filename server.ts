2fa13221e8e25904eacf617e19dd323b9fc812a5
Matis <mbaguelin@expertises-galtier.fr>
2024-01-22 14:13:13 +0100

    chore: add single quote rule in deno.json

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
-import { Application } from "https://deno.land/x/oak@v12.6.2/mod.ts";
+import { Application } from 'https://deno.land/x/oak@v12.6.2/mod.ts';
 
 const app = new Application();
 
 app.use((ctx) => {
-  ctx.response.body = "Hello world!";
+  ctx.response.body = 'Hello world!';
 });
 
 await app.listen({ port: 8000 });
